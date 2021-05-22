733064467b41f54811b86e0a460ae5a558baea5d
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2021-05-22 14:27:10 +0700

    Set up theme and navigation graph

--- app/src/main/java/com/example/suitevent/MainActivity.kt
+++ app/src/main/java/com/example/suitevent/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.suitevent
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.suitevent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.mainToolbar.toolbar)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.main_host_fragment).navigateUp()
+                || super.onSupportNavigateUp()
     }
 }
